74ccd294a15879862bd4b2622beb27534c0cbccb
YahiaEldakhakhny <yahia4360673@gmail.com>
2024-04-11 18:49:28 +0200

    cleanup

--- opt/cvx_try.py
+++ opt/cvx_try.py
@@ -4,7 +4,7 @@ import my_lib as ml
 
 
 # Load the training data
-training_data = ml.load_data('data_dir/lin100.csv')
+training_data = ml.load_data('small_lin.csv')
 [w, x] = ml.format_data(training_data)
 
 [A, B] = ml.get_opt_AB(w, x)
