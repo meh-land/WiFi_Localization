7495bb1a77c1524edb99a254d5864a1b64e34ef4
YahiaEldakhakhny <yahia4360673@gmail.com>
2024-04-12 19:25:50 +0200

    knn_perf skeleton

---
+++ knn/knn_perf.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+import gen_theoretical as gt
+import my_lib as ml
+from my_lib import plt
+import numpy as np
+import knn
+
+def MSE():
+    ...
+
+
+def data_size_perf():
+    ...
+
+def k_perf():
+    ...
